ec91bac490e553f0da76573661f71cf690558b09
Serjo <savenkoserhii@knu.ua>
2023-02-01 02:19:53 +0200

    to pep-8

--- code.py
+++ code.py
@@ -1,7 +1,5 @@
 from math import log2
 
-# from collections import Counter
-
 def get_file_size(filename):
     size = 0
     with open(filename, "rb") as f:
@@ -12,149 +10,166 @@ def get_file_size(filename):
             size += len(block)
     return size
 
-
-def entropy(inmputdict):
+def find_entropy_from(inmput_dict):
     sigma_m_i = 0
-    for i in inmputdict: # m = 33
+    for i in inmput_dict: # m = 33+
         try:
-            # print(i)
-            # print(log2(1/inmputdict[i]))
-            sigma_m_i += inmputdict[i] * log2(1/inmputdict[i])
+            sigma_m_i += inmput_dict[i] * log2(1/inmput_dict[i])
         except:
             continue
-    #print("H =", round(sigma_m_i, 5))
+    # print("H = ", round(sigma_m_i, 5))
     return sigma_m_i
 
-def Letterprobability(inmputdict, totallen):
-    # print("Total file len:", totallen)
-    UkrLetterP = {}
-    for i in inmputdict:
-        temp = round(inmputdict[i]/totallen, 8)
-        # print("letter '", i, "' show up", inmputdict[i], "times. Probability:",temp)
-        UkrLetterP.update({i: temp})
-    # print(UkrLetterP)
-    return UkrLetterP
+def probability_of_letter_to_appear(inmput_dict, total_number_of_char_in_text):
+    ukr_letter_probability_dict = {}
+    for i in inmput_dict:
+        temp = round(inmput_dict[i]/total_number_of_char_in_text, 8)
+        ukr_letter_probability_dict.update({i: temp})
+    return ukr_letter_probability_dict
 
 
-def zipsizeNotes(num): # size of file archivated in zip
+def zip_size_notes(input_num): # size of file archivated in zip
     from zipfile import ZipFile
-    zp = ZipFile("destextes/"+str(num)+".zip")
+    zp = ZipFile("destextes/"+str(input_num)+".zip")
     size = sum([info.file_size for info in zp.filelist])
     # zip_kb = float(size) / 1000  # kB
-    # print(zip_kb)
     zip_bytes = float(size)
-    # print(zip_bytes)
-    # zip_bits = float(size)*8 # bits
-    with ZipFile('destextes/'+str(num)+'.zip') as myzip:
+    # zip_bits = float(size) * 8 # bits
+    with ZipFile('destextes/'+str(input_num)+'.zip') as myzip:
         with myzip.open('1.txt') as myfile:
             for i in myfile:
                 print([i])
     return zip_bytes
 
-# def Base64encode(filename):
+# def Base64encode(filename): # will do from encode.py
 
-
-def readfileprobability (num, boolean_if_trueUkr):
-    if boolean_if_trueUkr:
-        file = open("destextes/"+str(num)+".txt", "r", encoding="utf8")
-        UkrLetterD = {"А": 0, "Б": 0, "В": 0, "Г": 0, "Ґ": 0, "Д": 0, "Е": 0, "Є": 0, "Ж": 0, "З": 0, "И": 0, "І": 0,
+def read_letter_in_file_probability (file_name_input_num, if_ukr_set_boolean_true):
+    if if_ukr_set_boolean_true:
+        file = open("destextes/"+str(file_name_input_num)+".txt", "r", encoding="utf8")
+        ukr_letter_amount_in_text_dict = {"А": 0, "Б": 0, "В": 0, "Г": 0, "Ґ": 0, "Д": 0, "Е": 0, "Є": 0, "Ж": 0, "З": 0, "И": 0, "І": 0,
                       "Ї": 0, "Й": 0, "К": 0, "Л": 0, "М": 0, "Н": 0, "О": 0, "П": 0,
                       "Р": 0, "С": 0, "Т": 0, "У": 0, "Ф": 0, "Х": 0, "Ц": 0, "Ч": 0, "Ш": 0, "Щ": 0, "Ь": 0, "Ю": 0,
                       "Я": 0,"а": 0, "б": 0, "в": 0, "г": 0, "ґ": 0, "д": 0, "е": 0, "є": 0, "ж": 0, "з": 0, "и": 0, "і": 0,
                       "ї": 0, "й": 0, "к": 0, "л": 0, "м": 0, "н": 0, "о": 0, "п": 0,
                       "р": 0, "с": 0, "т": 0, "у": 0, "ф": 0, "х": 0, "ц": 0, "ч": 0, "ш": 0, "щ": 0, "ь": 0, "ю": 0,
-                      "я": 0}  # "A":0, ,".": 0,",": 0,"!": 0,"?": 0,"-": 0 - do we even need it?
+                      "я": 0}  # 66
     else:
-        file = open("destextes/" + str(num) + "_base64.txt", "r", encoding="utf8")
-        UkrLetterD = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "F": 0, "G": 0, "H": 0, "I": 0, "J": 0, "K": 0, "L": 0,
+        file = open("destextes/" + str(file_name_input_num) + "_base64.txt", "r", encoding="utf8")
+        ukr_letter_amount_in_text_dict = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "F": 0, "G": 0, "H": 0, "I": 0, "J": 0, "K": 0, "L": 0,
                          "M": 0, "N": 0, "O": 0, "P": 0, "Q": 0, "R": 0, "S": 0, "T": 0, "U": 0, "V": 0, "W": 0, "X": 0,
                          "Y": 0, "Z": 0,"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0,
                          "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0,
                          "y": 0, "z": 0, "0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0,
-                         "+": 0, "/": 0, "=": 0} # Base64LetterD
-    # total alp len 33
-    totlen = 0
-    for i in file.read(): # why it read only as lovercase letters?
-        #print(i)
-        totlen += 1
+                         "+": 0, "/": 0, "=": 0} #  65
+
+    total_chars_in_text_amount_length_in_func = 0
+    for i in file.read():
+        total_chars_in_text_amount_length_in_func += 1
         try:
-            UkrLetterD[i] += 1
+            ukr_letter_amount_in_text_dict[i] += 1
         except:
-            UkrLetterD.update({i: 1}) # continue
-    alphabetPwr = len(UkrLetterD)
-    return UkrLetterD, totlen, alphabetPwr
+            continue
+            # ukr_letter_amount_in_text_dict.update({i: 1}) # continue
+    alphabet_power = len(ukr_letter_amount_in_text_dict)
+    return ukr_letter_amount_in_text_dict, total_chars_in_text_amount_length_in_func, alphabet_power
 
-    # entropy(Letterprobability(UkrLetterD, totlen))
-    # with open('destextes/1.txt', 'r', encoding="utf8") as file:
-    #     text = file.read().replace('\n', '')
-    # frequency = Counter(text)
-    # print("-------by counter-------")
-    # Letterprobability(frequency,totlen)
-    #
-    # return UkrLetterD # return symbols amount
-    # text = open("1.txt", 'r', encoding="cp1251").read()
-    # text
-def execute(LetterprobabilityAmount, totallen, power, filenum):
+def execute(amount_letters_to_appear, total_number_of_char_in_text, power, file_name_input_num, if_ukr_set_boolean_true):
+    file_type_list = ["txt","zip", "rar", "xz", "gz", "bz2"]
+    if if_ukr_set_boolean_true:
+        print("U.t total char len:",total_number_of_char_in_text)
+        print("Ukr alphabet power:", power)
+        print("Ukr letter amount:\n",amount_letters_to_appear)
+        probability_of_letter_to_appear_dict = probability_of_letter_to_appear(amount_letters_to_appear, total_number_of_char_in_text)
+        print("Ukr letter probability:\n", probability_of_letter_to_appear_dict)
+        entropy_no_round = find_entropy_from(probability_of_letter_to_appear_dict)
+        entropy_rounded_h_f = round(entropy_no_round,5)
+        print("H =",entropy_rounded_h_f)
+        amount_of_information_in_the_text = (entropy_rounded_h_f * total_number_of_char_in_text)*0.125 # /8
+        print("Information:",round(amount_of_information_in_the_text, 3), "bytes")
+        file_size = get_file_size("destextes/"+str(file_name_input_num)+'.'+file_type_list[0])
+        print("file size:",file_size, "bytes")
+        zip_size = get_file_size("destextes/"+str(file_name_input_num)+'.'+file_type_list[1])
+        print("zipped size:", zip_size, "bytes")
+        rar_size = get_file_size("destextes/" + str(file_name_input_num) + '.' + file_type_list[2])
+        print("rared size:", rar_size, "bytes")
+        xz_size = get_file_size("destextes/" + str(file_name_input_num) + '.' + file_type_list[3])
+        print("xzed size:", xz_size, "bytes")
+        gzip_size = get_file_size("destextes/" + str(file_name_input_num) + '.' + file_type_list[4])
+        print("gzipped size:", gzip_size, "bytes")
+        bzip_size = get_file_size("destextes/" + str(file_name_input_num) + '.' + file_type_list[5])
+        print("bzipped size:", bzip_size, "bytes")
+        base64_file_size = get_file_size("destextes/" + str(file_name_input_num) + '_base64.' + file_type_list[0])
+        print("based64 size:", base64_file_size, "bytes")
+    else:
+        print('---------------------------------------')
+        print("total b64 char len:",total_number_of_char_in_text)
+        print("b64 alphabet power:", power)
+        print("b64 letter amount:\n",amount_letters_to_appear)
+        probability_of_letter_to_appear_dict = probability_of_letter_to_appear(amount_letters_to_appear, total_number_of_char_in_text)
+        print("b64 letter probability:\n", probability_of_letter_to_appear_dict)
+        entropy_no_round = find_entropy_from(probability_of_letter_to_appear_dict)
+        entropy_rounded_h_f = round(entropy_no_round,5)
+        print("b64 H =",entropy_rounded_h_f)
+        amount_of_information_in_the_text = (entropy_rounded_h_f * total_number_of_char_in_text)*0.125 # /8
+        print("b64 Information:",round(amount_of_information_in_the_text, 3), "bytes")
+        file_size = get_file_size("destextes/"+str(file_name_input_num)+'_base64.'+file_type_list[0])
+        print("b64 file size:",file_size, "bytes")
+        zip_size = get_file_size("destextes/"+str(file_name_input_num)+'_base64.'+file_type_list[1])
+        print("b64 zipped size:", zip_size, "bytes")
+        rar_size = get_file_size("destextes/" + str(file_name_input_num) + '_base64.' + file_type_list[2])
+        print("b64 rared size:", rar_size, "bytes")
+        xz_size = get_file_size("destextes/" + str(file_name_input_num) + '_base64.' + file_type_list[3])
+        print("b64 xzed size:", xz_size, "bytes")
+        gzip_size = get_file_size("destextes/" + str(file_name_input_num) + '_base64.' + file_type_list[4])
+        print("b64 gzipped size:", gzip_size, "bytes")
+        bzip_size = get_file_size("destextes/" + str(file_name_input_num) + '_base64.' + file_type_list[5])
+        print("b64 bzipped size:", bzip_size, "bytes")
+        base64_file_size = get_file_size("destextes/" + str(file_name_input_num) + '_base64.' + file_type_list[0])
+        print("b64 based64 size:", base64_file_size, "bytes")
+    return probability_of_letter_to_appear_dict, entropy_rounded_h_f, amount_of_information_in_the_text, file_size, zip_size, rar_size, xz_size, gzip_size, bzip_size, base64_file_size
 
-    filetype = ["txt","zip", "rar", "xz", "gz", "bz2"]
-
-
-    print("total char len:",totallen)
-    print("alphabet power:", power)
-    print("letter amount:\n",LetterprobabilityAmount)
-    LetterprobabilityDict = Letterprobability(LetterprobabilityAmount, totallen)
-    print("letter probability:\n", LetterprobabilityDict)
-    test3 = entropy(LetterprobabilityDict)
-    H_f = round(test3,5)
-    print("H =",H_f)
-    infomount = (H_f * totallen)*0.125 # /8
-    print("Information:",round(infomount, 3), "bytes")
-    filesize = get_file_size("destextes/"+str(filenum)+"."+filetype[0])
-    print("file size:",filesize, "bytes")
-    zipsize = get_file_size("destextes/"+str(filenum)+'.'+filetype[1])
-    print("zipped size:", zipsize, "bytes")
-    rarsize = get_file_size("destextes/" + str(filenum) + '.' + filetype[2])
-    print("rared size:", rarsize, "bytes")
-    xzsize = get_file_size("destextes/" + str(filenum) + '.' + filetype[3])
-    print("xzed size:", xzsize, "bytes")
-    gzipsize = get_file_size("destextes/" + str(filenum) + '.' + filetype[4])
-    print("gzipped size:", gzipsize, "bytes")
-    bzipsize = get_file_size("destextes/" + str(filenum) + '.' + filetype[5])
-    print("bzipped size:", bzipsize, "bytes")
-    base64fz = get_file_size("destextes/" + str(filenum) + '_base64.' + filetype[0])
-    print("based64 size:", base64fz, "bytes")
-    return LetterprobabilityAmount, LetterprobabilityDict, H_f, infomount, filesize, zipsize, rarsize, xzsize, gzipsize, bzipsize, base64fz
-
-def preettyoutput(LetterprobabilityAmount, LetterprobabilityDict, H_f, infomount, filesize, zipsize, rarsize, xzsize, gzipsize, bzipsize, base64fsz):
+def preetty_output(amount_letters_to_appear, probability_of_letter_to_appear_dict, entropy_rounded_h_f, amount_of_information_in_the_text, file_size, zip_size,
+                   rar_size, xz_size, gzip_size, bzip_size, base64fsz, entropy_rounded_base64_h_f,amount_of_information_in_the_base64_text, base64_zip_size, base64_rar_size,
+                   base64_xz_size, base64_gzip_size, base64_bzip_size): # 18
     from prettytable import PrettyTable
     table = PrettyTable()
     table.field_names = ["Letter", "Amount", "Probability"]
-    for i in LetterprobabilityDict:
-        table.add_row(["'"+i+"'", LetterprobabilityAmount[i], LetterprobabilityDict[i]])
+    for i in probability_of_letter_to_appear_dict:
+        table.add_row(["'"+i+"'", amount_letters_to_appear[i], probability_of_letter_to_appear_dict[i]])
         # print(i,"-",test1[i], "-", test2[i])
     table2 = PrettyTable()
     table2.field_names = ["Variable name","Value", "dT"]
-    table2.add_row(["Average UKR entropy",H_f, ''])
+    table2.add_row(["Average UKR entropy",entropy_rounded_h_f, ''])
+    table2.add_row(["Average B64 entropy", entropy_rounded_base64_h_f, ''])
     table2.add_row(["---------", 5.51, ' -----'])
-    table2.add_row(["Information Amount", (round(infomount, 3))," bytes"])
-    table2.add_row(["Actual File size", (filesize) ," bytes"])
-    table2.add_row(["Zipped File size", (round(zipsize, 4))," bytes"])
-    table2.add_row(["Rared File size", (round(rarsize, 4))," bytes"])
-    table2.add_row(["Xzed File size", (round(xzsize, 4))," bytes"])
-    table2.add_row(["Gzipped File size", round(gzipsize, 4)," bytes"])
-    table2.add_row(["Bzipped File size", round(bzipsize, 4)," bytes"])
+    table2.add_row(["UkrT. Information Amount", (round(amount_of_information_in_the_text, 3))," bytes"])
+    table2.add_row(["B64T. Information Amount", (round(amount_of_information_in_the_base64_text, 3)), " bytes"])
+    table2.add_row(["Actual File size", (file_size) ," bytes"])
+    table2.add_row(["Zipped File size", (round(zip_size, 4))," bytes"])
+    table2.add_row(["B64 Zipped File size", (round(base64_zip_size, 4)), " bytes"])
+    table2.add_row(["Rared File size", (round(rar_size, 4))," bytes"])
+    table2.add_row(["B64 Rared File size", (round(base64_rar_size, 4)), " bytes"])
+    table2.add_row(["Xzed File size", (round(xz_size, 4))," bytes"])
+    table2.add_row(["B64 Xzed File size", (round(base64_xz_size, 4)), " bytes"])
+    table2.add_row(["Gzipped File size", round(gzip_size, 4)," bytes"])
+    table2.add_row(["B64 Gzipped File size", round(base64_gzip_size, 4), " bytes"])
+    table2.add_row(["Bzipped File size", round(bzip_size, 4)," bytes"])
+    table2.add_row(["B64 Bzipped File size", round(base64_bzip_size, 4), " bytes"])
     table2.add_row(["Based64 File size", round(base64fsz, 4), " bytes"])
-    table2.sortby = 'Value'
-    table2.reversesort = True
+    # table2.sortby = 'Value'
+    # table2.reversesort = True
     print(table)
     print(table2)
-filenum = 2
-LetterprobabilityAmount, totallen, power = readfileprobability(filenum, True)
-test1, test2, H_f, infomount, filesize,zipsize, rarsize, xzsize, gzipsize, bzipsize, base64fsz = execute(LetterprobabilityAmount, totallen, power, filenum)
-preettyoutput(test1, test2, H_f, infomount, filesize,zipsize, rarsize, xzsize, gzipsize, bzipsize, base64fsz)
-# print(bin("к"), "к")
-# s = input("-----")
-# if s != "asd":
-#    print("end")
-# else:
-#    print("sad")
+
+file_name_input_num = 2
+amount_letters_to_appear, total_number_of_char_in_text, alph_power = read_letter_in_file_probability(file_name_input_num, True)
+probability_of_letter_to_appear_dict, entropy_rounded_h_f, amount_of_information_in_the_text, file_size,zip_size, rar_size, xz_size, gzip_size, bzip_size, base64_file_size = execute(amount_letters_to_appear, total_number_of_char_in_text, alph_power, file_name_input_num, True)
+# preetty_output(amount_letters_to_appear, probability_of_letter_to_appear_dict, entropy_rounded_h_f, amount_of_information_in_the_text, file_size,zip_size, rar_size, xz_size, gzip_size, bzip_size, base64_file_size)
+
+amount_letters_to_appear_base64, total_number_of_char_in_text_base64, base64_alph_power = read_letter_in_file_probability(file_name_input_num, False)
+#print(amount_letters_to_appear_base64)
+probability_of_letter_to_appear_base64_dict, entropy_rounded_base64_h_f, amount_of_information_in_the_base64_text, base64_file_size,base64_zip_size, base64_rar_size, base64_xz_size, base64_gzip_size, base64_bzip_size, base64_file_size2 = execute(amount_letters_to_appear_base64, total_number_of_char_in_text_base64, base64_alph_power, file_name_input_num, False)
+preetty_output(amount_letters_to_appear, probability_of_letter_to_appear_dict, entropy_rounded_h_f, amount_of_information_in_the_text,
+               file_size,zip_size, rar_size, xz_size, gzip_size, bzip_size, base64_file_size,
+               entropy_rounded_base64_h_f, amount_of_information_in_the_base64_text,
+               base64_zip_size, base64_rar_size, base64_xz_size, base64_gzip_size,base64_bzip_size) # 20
--- encode.py
+++ encode.py
@@ -1,9 +1,7 @@
-# from code import get_file_size
-
 
 BASE64_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
             #   0123456789              24          36              52
-def readfile(num, type, bool): # false if base64encoded text
+def read_file(num, type, bool): # false if base64encoded text
     if bool:
         with open("destextes/" + str(num) + "." + str(type), "r", encoding="utf8") as file:
             total_text = ""
@@ -24,13 +22,13 @@ def readfile(num, type, bool): # false if base64encoded text
                 # print(i)
                 if i not in LetterD:
                     continue
-                else:           ### HERE WE GO????????????
+                else:           ### HERE WE GO!!!!!!!!!!!!!!!!!!!
                     total_text += i
                     totlen += 1
                     try:
                         LetterD[i] += 1
                     except:
-                        continue  # LetterD.update({i: 1})  # continue'''
+                        continue  # LetterD.update({i: 1})  # continue
         alphabetPwr = len(LetterD)
         #total_text = file.read()
     else:
@@ -56,53 +54,25 @@ def readfile(num, type, bool): # false if base64encoded text
             try:
                 LetterD[i] += 1
             except:
-                continue  # LetterD.update({i: 1})  # continue'''
+                continue  # LetterD.update({i: 1})  # continue
         alphabetPwr = len(LetterD)
 
     return total_text,LetterD, totlen, alphabetPwr
 
-def chuckprinter(chunks):
-    if len(chunks)%4 == 0:
-        for i in range(0,len(chunks)):
-            # continue
-            print(i)
-            print(chunks[i],':',chunks[i+1],':',chunks[i+2],':',chunks[i+3])  # ,':',chunks[i+4],':',chunks[i+5]
-            if i == len(chunks) - 4:
-                if len(chunks[i + 3])<6:
-                    print("two bytes -> '",chunks[i+3],"' now will be '='", )
-    else:
-        for i in range(0,len(chunks)-4, 4):
-            # continue
-            print(i)
-            print(chunks[i],':',chunks[i+1],':',chunks[i+2],':',chunks[i+3])  # ,':',chunks[i+4],':',chunks[i+5]
-        if (len(chunks)) % 4 != 0:
-            if (len(chunks) - 2) % 4 != 0:
-                print('<----000---->')
-            else:
-                print(len(chunks) - 2)
-                print(chunks[len(chunks) - 2], ':', chunks[len(chunks) - 1], "- one byte (2 =)")
-        else:
-            print(len(chunks) - 4)
-            print(chunks[len(chunks) - 4], ':', chunks[len(chunks) - 3], ':', chunks[len(chunks) - 2], ':',
-                  chunks[len(chunks) - 1], "- two bytes (1 =)")
-def textbase64(data):
+def text_base_64(data):
     result = ""
-
     data_encoded = data.encode("utf-8", errors='replace') # encoding='unicode_escape' # hexadecimal codes
-    #binary_data = ("".join(format(byte, 'b') for byte in data_encoded))
-    binary_data = ''
-    for i in data_encoded:
-        local = format(i,'b')
-        binary_data += local
-
-    print(data_encoded)
+    binary_data = ("".join(format(byte, 'b') for byte in data_encoded))
+    #binary_data = ''
+    #for i in data_encoded:
+    #    local = format(i,'b')
+    #    binary_data += local
+    #print(data_encoded)
     #print(binary_data)
-
     chunks = [binary_data[i:i + 24] for i in range(0, len(binary_data), 24)]
-    print(chunks)
+    #print(chunks)
 
     for chunk in chunks:
-        # print("chunk that decoding:" + chunk)
         if chunk != chunks[len(chunks)-1]: # last chunk
             index = int(chunk[0:6], 2)
             index2 = int(chunk[6:12], 2)
@@ -112,9 +82,6 @@ def textbase64(data):
             result += BASE64_CHARS[index2]
             result += BASE64_CHARS[index3]
             result += BASE64_CHARS[index4]
-            #print("index = {}, index2 = {}, index3={}, index4 = {}".format(index, index2, index3, index4))
-            #print("That means symbols {} - {} - {} - {}".format(BASE64_CHARS[index], BASE64_CHARS[index2],
-            #                                                    BASE64_CHARS[index3], BASE64_CHARS[index4]))
         else:
             if len(chunks[len(chunks) - 1]) == 24:
                 index = int(chunk[0:6], 2)
@@ -134,8 +101,6 @@ def textbase64(data):
                     result += BASE64_CHARS[index2]
                     result += BASE64_CHARS[index3]
                     result += '='
-                    #print("index = {}, index2 = {}, index3={}, index4 = '='".format(index, index2,index3))
-                    #print("That means symbols {} - {} - {} - {}".format(BASE64_CHARS[index],BASE64_CHARS[index2],BASE64_CHARS[index3],'='))
                 else:
                     if len(chunks[len(chunks) - 1]) >= 12: # index - index2
                         index = int(chunk[0:6], 2)
@@ -144,9 +109,6 @@ def textbase64(data):
                         result += BASE64_CHARS[index2]
                         result += '='
                         result += '='
-                        #print("index = {}, index2 = {}, index3={}, index4 = {}".format(index, index2, index3, index4))
-                        #print("That means symbols {} - {} - {} - {}".format(BASE64_CHARS[index], BASE64_CHARS[index2],
-                        #                                                   '=', '='))
                     else:
                         try:
                             index = int(chunk[0:6], 2)
@@ -155,41 +117,25 @@ def textbase64(data):
                             print("error")
     return result
 
-def frBtoBin(): # temp
-    sus = int('110111', 2) # temp
+def fr_to_bin(temp):
+    sus = int('temp', 2) # temp
     print(sus)
     local = BASE64_CHARS[sus]
     print(local)
     susbin = format(sus, '06b')
     print(susbin)
-# frBtoBin()
-
-# this code gen.s  0KODQsdGD0LTRjLdGP0LrRltC5g0L/RgNCw0LLQvtCy0ZbQ
-# but services     0KMg0LHRg9C00Ywt0Y/QutGW0Lkg0L/RgNCw0LLQvtCy0ZbQ
-# resources
-filenum = 2
-data,UkrLetterD, Ukrtotallen, UkrAlphabet_power = readfile(filenum,"txt", True)
-print(data)
-print("----")
-
-#print(UkrLetterD)
 
 
-base64_text = textbase64(data)
-print("  "+ base64_text)
-print("----")
-
-import base64
-bnotmine = base64.b64encode(bytes(data, 'utf-8'))
-print(bnotmine) # "Base64 encoded text:",
-
-try:
-    f = open("destextes/"+str(filenum)+"_base64.txt", "w")
-    f.writelines(base64_text)
-    f.close()
-except:
-    print("error")
-
-
-# dataBase,UkrLetterBase, UkrtotallenBase, BaseAlphabet_power = readfile(filenum,"txt", False)
-
+def encode_py_executer(filenum):
+    # filenum = 4
+    data,UkrLetterD, Ukrtotallen, UkrAlphabet_power = read_file(filenum,"txt", True)
+    base64_text = text_base_64(data)
+    # import base64
+    # bnotmine = base64.b64encode(bytes(data, 'utf-8'))
+    # print(bnotmine) # "Base64 encoded text:",
+    try:
+        f = open("destextes/"+str(filenum)+"_base64.txt", "w")
+        f.writelines(base64_text)
+        f.close()
+    except:
+        print("encode_py_executer error (in encode.py)")
